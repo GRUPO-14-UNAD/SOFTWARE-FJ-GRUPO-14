772ac664513aa230a16565ad7b8255aadd015623
Juan David Crespo c. <jdcrespoc@unadvirtual.edu.co>
2026-06-19 18:29:58 -0500

    prueba

--- grupo14-unad.py
+++ grupo14-unad.py
@@ -1,5 +1,4 @@
 # SOFTWARE-FJ-GRUPO-14
 #Sistema Integral de Gestión de Clientes, Servicios y  Reservas
 # CREADDORES: Juan David Crespo Carpio
-# Versión 1.0 - Proyecto inicial
 # fecha de inicio: 19 de junio 2026
